232deaed945c2e1e08c1bb39108a59133fd92160
todeskurve <47814519+todeskurve@users.noreply.github.com>
2024-09-09 11:38:09 +0200

    overwrite stored data before forgetting about it in the references

--- Lib/shelve.py
+++ Lib/shelve.py
@@ -20,6 +20,8 @@ object):
                         # access returns a *copy* of the entry!
         del d[key]      # delete data stored at key (raises KeyError
                         # if no such key)
+                        # removing data with del() or .pop() does not affect
+                        # stored files in every case
         flag = key in d # true if the key exists
         list = d.keys() # a list of all existing keys (slow!)
 
@@ -117,7 +119,12 @@ class Shelf(collections.abc.MutableMapping):
         return value
 
     def __setitem__(self, key, value):
+        #print(self.dict.keys())
+        if key.encode(self.keyencoding) in self.dict.keys():
+            self.dict[key.encode(self.keyencoding)] = " " * len(self.dict[key.encode(self.keyencoding)])
         if self.writeback:
+            if key in self.cache.keys():
+                self.cache[key] = " " * len(self.cache[key])
             self.cache[key] = value
         f = BytesIO()
         p = Pickler(f, self._protocol)
@@ -125,8 +132,13 @@ class Shelf(collections.abc.MutableMapping):
         self.dict[key.encode(self.keyencoding)] = f.getvalue()
 
     def __delitem__(self, key):
+        #print(self.dict.keys())
+        if key.encode(self.keyencoding) in self.dict.keys():
+            self.dict[key.encode(self.keyencoding)] = " " * len(self.dict[key.encode(self.keyencoding)])
         del self.dict[key.encode(self.keyencoding)]
         try:
+            if key in self.cache.keys():
+                self.cache[key] = " " * len(self.cache[key])
             del self.cache[key]
         except KeyError:
             pass
@@ -225,13 +237,6 @@ class DbfilenameShelf(Shelf):
     def __init__(self, filename, flag='c', protocol=None, writeback=False):
         import dbm
         Shelf.__init__(self, dbm.open(filename, flag), protocol, writeback)
-
-    def clear(self):
-        """Remove all items from the shelf."""
-        # Call through to the clear method on dbm-backed shelves.
-        # see https://github.com/python/cpython/issues/107089
-        self.cache.clear()
-        self.dict.clear()
 
 
 def open(filename, flag='c', protocol=None, writeback=False):
